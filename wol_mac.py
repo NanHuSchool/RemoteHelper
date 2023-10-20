508528050429c3c21967a892583eb4d8dc62eaf0
mochenya <mochen20070702@gmail.com>
2023-10-20 11:06:40 +0800

    判断是否处于Pyinstaller

--- wol_mac.py
+++ wol_mac.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import csv
 from wakeonlan import send_magic_packet
 
 def wake_mac(csv_path):
+    if getattr(sys, 'frozen', False):
+        print('当前运行是用 pyinstaller 打包的环境')
+        csv_abspath = os.path.join(os.path.dirname(__file__), csv_path)
+        print(f"csv文件：{csv_abspath}")
     # 判断csv文件是否存在
-    if os.path.exists(csv_path):
+    elif os.path.exists(csv_path):
         csv_abspath = os.path.abspath(csv_path)
         print(f"csv文件：{csv_abspath}")
     else:
         print(f"找不到csv文件： {csv_path}")
-        exit()
+        sys.exit()
 
     # 读取文件
     with open(csv_abspath, mode="r", encoding="utf-8") as f:
@@ -35,7 +40,7 @@ def wake_mac(csv_path):
             send_magic_packet(mac_address_two)
 
 def main():
-    wake_mac("mac.csv")
+    wake_mac(".\mac.cv")
 
 if __name__ == '__main__':
     main()
