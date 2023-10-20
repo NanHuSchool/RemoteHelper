9f5db07de8dcb4f51c1519215af6fe2673d23864
mochenya <mochen20070702@gmail.com>
2023-10-20 13:24:45 +0800

    quickset: 开启防火墙

--- quickset.c
+++ quickset.c
@@ -9,6 +9,8 @@ int main(){
     system("reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\SharedAccess\\Parameters\\FirewallPolicy\\StandardProfile /v EnableFirewall /t REG_DWORD /d 0 /f");
     system("reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\SharedAccess\\Parameters\\FirewallPolicy\\PublicProfile /v EnableFirewall /t REG_DWORD /d 0 /f");
     system("reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\SharedAccess\\Parameters\\FirewallPolicy\\DomainProfile /v EnableFirewall /t REG_DWORD /d 0 /f");
+    // 防火墙放行远程桌面3389默认端口
+    system("netsh advfirewall firewall add rule name=\"Open Port 3389\" dir=in action=allow protocol=TCP localport=3389");
     // Windows Defender 关闭
     system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows Defender\" /v DisableAntiSpyware /t REG_DWORD /d 1 /f");
     system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection\" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f");
