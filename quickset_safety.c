6f0986b547e60228c138c1e5ea57ecc1791c05b4
mochenya <mochen20070702@gmail.com>
2023-10-21 21:13:06 +0800

    quickset_safety: 开启Denfender

--- quickset_safety.c
+++ quickset_safety.c
@@ -8,6 +8,9 @@ int main(){
     system("netsh advfirewall set allprofiles state off");
     // 防火墙放行远程桌面3389默认端口
     system("netsh advfirewall firewall add rule name=\"Open Port 3389\" dir=in action=allow protocol=TCP localport=3389");
+    // Windows Defender 开启
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows Defender\" /v DisableAntiSpyware /t REG_DWORD /d 2 /f");
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection\" /v DisableRealtimeMonitoring /t REG_DWORD /d 2 /f");
     // 开启远程桌面
     system("reg add \"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Terminal Server\" /v fDenyTSConnections /t REG_DWORD /d 0 /f");
     // 本地安全策略
@@ -28,6 +31,9 @@ int main(){
     // 刷新DNS服务
     system("ipconfig /flushdns");
     // 开启防火墙
+    system("reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\SharedAccess\\Parameters\\FirewallPolicy\\StandardProfile /v EnableFirewall /t REG_DWORD /d 1 /f");
+    system("reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\SharedAccess\\Parameters\\FirewallPolicy\\PublicProfile /v EnableFirewall /t REG_DWORD /d 1 /f");
+    system("reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\SharedAccess\\Parameters\\FirewallPolicy\\DomainProfile /v EnableFirewall /t REG_DWORD /d 1 /f");
     system("netsh advfirewall set allprofiles state on");
     // 重启系统
     // system("shutdown /r /t 0");
