61bdbbbb7a8ffd2efe072093811e5b839f96e82d
mochenya <mochen20070702@gmail.com>
2024-01-19 13:15:07 +0800

    rdp_generate: 规范代码格式

--- rdp_generate.py
+++ rdp_generate.py
@@ -1,56 +1,58 @@
 # -*- coding: utf-8 -*-
+#
+# GNU General Public License v3.0
+# Copyright (C) 2024 OpenNanHu
+# 将当前目录下csv文件中的信息生成rdp文件
 import os
 import csv
 
-def main(cvs_name):
+def generate(cvs_name):
     # csv数据文件位置
     csv_path = os.path.join(os.path.dirname(__file__), cvs_name)
     # 检测文件
     if os.path.exists(csv_path):
-        print(f"数据位置： {csv_path}")
+        print(f"数据位置：{csv_path}")
     else:
-        print(f"找不到文件： {cvs_name}")
-        exit(1)
+        print(f"找不到文件：{cvs_name}")
+        exit()
 
     # 创建rdp保存文件夹
-    savedir_name = "rdp"
-    savedir_path = os.path.join(os.path.dirname(__file__), savedir_name)
-    if os.path.exists(savedir_path):
-        print(f"保存文件夹: {savedir_name}")
-    else:
-        try:
-            os.makedirs(savedir_path, exist_ok=True)
-            print(f"创建保存文件夹成功：{savedir_path}")
-        except OSError as e:
-            exit(1)
+    out_dir = "rdp"
+    out_path = os.path.join(os.path.dirname(__file__), out_dir)
+    try:
+        if not os.path.exists(out_path):
+            os.makedirs(out_path, exist_ok=True)
+            print(f"创建保存文件夹成功：{out_path}")
+    except OSError as e:
+            print(f"创建保存文件夹失败：{e}")
+            exit()
 
     # 打开CVS文件并读取
-    with open(csv_path, mode="r", encoding="utf-8") as f:
+    with open(csv_path, mode="+r", encoding="utf-8") as f:
         csv_reader = csv.reader(f)
         # 跳过标题行
         next(csv_reader)
         # 每行读取，并获取相关信息
         for r in csv_reader:
-            # 设备信息 IP Mac地址1 Mac地址2
+            # 设备名称,IP,MAC1,MAC2,Account,WOL_Status
             device_name = r[0]
             ip_address = r[1]
-            mac_address_one = r[2]
-            mac_address_two = r[3]
+            username=r[4]
             # rdp文件变量
             rdp_name = device_name + ".rdp"
-            rdp_path = os.path.join(savedir_path, rdp_name)
+            rdp_path = os.path.join(out_path, rdp_name)
             # 写入rdp
             context = f"""screen mode id:i:2
 use multimon:i:0
 desktopwidth:i:1920
 desktopheight:i:1080
 session bpp:i:32
-winposstr:s:0,3,0,0,800,600
+winposstr:s:0,1,72,269,932,1020
 compression:i:1
-keyboardhook:i:2
+keyboardhook:i:0
 audiocapturemode:i:0
 videoplaybackmode:i:1
-connection type:i:7
+connection type:i:6
 networkautodetect:i:1
 bandwidthautodetect:i:1
 displayconnectionbar:i:1
@@ -66,16 +68,14 @@ bitmapcachepersistenable:i:1
 full address:s:{ip_address}
 audiomode:i:0
 redirectprinters:i:1
-redirectlocation:i:1
-redirectcomports:i:1
+redirectcomports:i:0
 redirectsmartcards:i:1
 redirectwebauthn:i:1
 redirectclipboard:i:1
 redirectposdevices:i:0
-drivestoredirect:s:*
 autoreconnection enabled:i:1
 authentication level:i:0
-prompt for credentials:i:0
+prompt for credentials:i:1
 negotiate security layer:i:1
 remoteapplicationmode:i:0
 alternate shell:s:
@@ -90,15 +90,18 @@ use redirection server name:i:0
 rdgiskdcproxy:i:0
 kdcproxyname:s:
 enablerdsaadauth:i:0
-username:s:seewo
+drivestoredirect:s:*
+username:s:{username}
 """
             try:
-                with open(rdp_path, mode="w", newline="", encoding="utf-8") as rdp_f:
-                    rdp_f.write(context)
+                with open(rdp_path, mode="w", newline="", encoding="utf-8") as f:
+                    f.write(context)
+                    f.close
                     print(f"rdp: {rdp_path}")
             except Exception as e:
-                    print(f"{rdp_name} 保存失败")
+                    print(f"{rdp_name} 保存失败：{e}")
 
 
 if __name__ == "__main__":
-    main("mac.csv")
+    generate("mac.csv")
+    generate("mac_server.csv")
